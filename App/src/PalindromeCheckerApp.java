c1742a617144048bc536a1e2ce7905803adfe370
Vishesh-Jhabak <visheshjhabak@gmail.com>
2026-02-20 10:54:19 +0530

    checking palindrome by giving reverse text

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to check if it is a palindrome: ");
+        String original = scanner.nextLine();
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println(" The string " + original + " is a Palindrome .");
+        } else {
+            System.out.println(" The string  "+ original +  " is not a Palindrome ");
+        }
+        scanner.close();
+    }
+}
