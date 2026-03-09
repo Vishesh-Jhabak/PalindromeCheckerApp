3ca50580358cc11d9d9090a17f5200f31bb657dc
Vishesh-Jhabak <visheshjhabak@gmail.com>
2026-03-09 08:43:35 +0530

    UC12-PalindromeStrategypattern

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,28 +1,75 @@
-/*
- * Application entry point.
- *
- * This is the first method executed by the JVM
- * when the program starts
- *
- * @author Developer
- * @Version 1.0
- */
+import java.util.*;
 
-import java.util.Scanner;
+interface PalindromeStrategy {
+    boolean check(String s);
+}
+
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) stack.push(c);
+        for (char c : s.toCharArray())
+            if (c != stack.pop()) return false;
+        return true ;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String s) {
+        Deque<Character> dq = new ArrayDeque<>();
+        for (char c : s.toCharArray()) dq.add(c);
+
+        while (dq.size() > 1)
+            if (dq.removeFirst() != dq.removeLast()) return false;
+
+        return true;
+    }
+}
+
+// Context class
+class PalindromeChecker {
+    private PalindromeStrategy strategy;
+
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean checkPalindrome(String s) {
+        s = s.replaceAll("\\s+", "").toLowerCase();
+        return strategy.check(s);
+    }
+}
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the srting : ");
-        String original = sc.nextLine();
-        String reversed ="";
-        for (int i =original.length() -1; i>=0; i--){
-            reversed = reversed + original.charAt(i);
-        }
-        if (original.equals(reversed)) {
-            System.out.println(" The string \"" + original + "\" is a Palindrome" );
-        } else {
-            System.out.println(" The string \"" + original + "\" is not a Palindrome" );
-        }
+
+        System.out.println("1. Stack Strategy");
+        System.out.println("2. Deque Strategy");
+        System.out.print("Choose strategy: ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        PalindromeStrategy strategy;
+
+        if (choice == 1)
+            strategy = new StackStrategy();
+        else
+            strategy = new DequeStrategy();
+
+        PalindromeChecker checker = new PalindromeChecker(strategy);
+
+        if (checker.checkPalindrome(input))
+            System.out.println("Palindrome ");
+        else
+            System.out.println("Not a Palindrome ");
+
         sc.close();
     }
 }
